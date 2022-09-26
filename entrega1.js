94bbf014ea408334b13b3375bdeb08ab825a4d85
Jose Recart <joserecart91@gmail.com>
2022-09-26 12:13:36 -0300

    1ra entrega js

---
+++ entrega1.js
@@ -0,0 +1,53 @@
+//EJERCICIO VARIABLE-FUNCION
+
+// let num1 = parseInt(prompt("ingrese un valor numerico: "))
+// let num2 = 7
+
+// function sumarNumeros(a, b) {
+//     let resultado = num1 + num2 
+//     return resultado
+// } 
+
+// alert(sumarNumeros(num1, num2))
+
+
+
+
+
+// EJERCICIO CICLO FOR : imprimir por consola numeros del 1 al 100
+
+// for (i = 0; i <= 100; i++) {
+//     console.log(i)
+// }
+
+
+
+
+
+
+// EJERCICIO CON WHILE
+
+// const marcasDeAuto = ["Suzuki", "Nissan", "Toyota", "Mazda", "Subaru", "Daihatsu"];
+// let i = 0;
+// let text = "";
+
+// while (marcasDeAuto[i]) {
+//     text += marcasDeAuto[i];
+//     i++;
+//     console.log(marcasDeAuto[i])
+// }
+
+
+
+
+
+
+// EJERCITANDO CONDICIONALES
+
+// let salsa = 'Ketchup';
+
+// if (salsa) {
+//     console.log('Tenemos Ketchup para las hamburguesas!!');
+// } else {
+//     console.log('Ya no nos queda Ketchup :( ');
+// }
